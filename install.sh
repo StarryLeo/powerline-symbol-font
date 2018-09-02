ba8201a4c6c6c141f4ba544961f9d99475f5e939
StarryLeo <starryskymayuyu@gmail.com>
2018-09-02 11:24:42 +0800

    Update symbol

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Set source and target directories
-powerline_fonts_dir="$( cd "$( dirname "$0" )" && pwd )"
+powerline_symbol_font_dir="$( cd "$( dirname "$0" )" && pwd )"
 
 # if an argument is given it is used to select which fonts to install
 prefix="$1"
@@ -16,8 +16,8 @@ else
 fi
 
 # Copy all fonts to user fonts directory
-echo "Copying fonts..."
-find "$powerline_fonts_dir" \( -name "$prefix*.[ot]tf" -or -name "$prefix*.pcf.gz" \) -type f -print0 | xargs -0 -n1 -I % cp "%" "$font_dir/"
+echo "Copying font..."
+find "$powerline_symbol_font_dir" \( -name "$prefix*.[ot]tf" -or -name "$prefix*.pcf.gz" \) -type f -print0 | xargs -0 -n1 -I % cp "%" "$font_dir/"
 
 # Reset font cache on Linux
 if which fc-cache >/dev/null 2>&1 ; then
@@ -25,4 +25,4 @@ if which fc-cache >/dev/null 2>&1 ; then
     fc-cache -f "$font_dir"
 fi
 
-echo "Powerline fonts installed to $font_dir"
+echo "Powerline symbol font installed to $font_dir"
--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Set source and target directories
-powerline_fonts_dir="$( cd "$( dirname "$0" )" && pwd )"
+powerline_symbol_font_dir="$( cd "$( dirname "$0" )" && pwd )"
 
 # if an argument is given it is used to select which fonts to uninstall
 prefix="$1"
@@ -13,14 +13,14 @@ else
   # Linux
   font_dir="$HOME/.local/share/fonts"
   if test ! -d "$font_dir" ; then
-    echo "It seems there are no powerline fonts installed on your system. Uninstall not needed."
+    echo "It seems there are no powerline symbol font installed on your system. Uninstall not needed."
     exit 0
   fi
 fi
 
 # Remove all fonts from user fonts directory
-echo "Removing fonts..."
-find "$powerline_fonts_dir" \( -name "$prefix*.[ot]tf" -or -name "$prefix*.pcf.gz" \) -type f -print0 | xargs -n1 -0 -I % sh -c "rm -f \"\$0/\${1##*/}\"" "$font_dir" %
+echo "Removing font..."
+find "$powerline_symbol_font_dir" \( -name "$prefix*.[ot]tf" -or -name "$prefix*.pcf.gz" \) -type f -print0 | xargs -n1 -0 -I % sh -c "rm -f \"\$0/\${1##*/}\"" "$font_dir" %
 
 # Reset font cache on Linux
 if which fc-cache >/dev/null 2>&1 ; then
@@ -28,4 +28,4 @@ if which fc-cache >/dev/null 2>&1 ; then
     fc-cache -f "$font_dir"
 fi
 
-echo "Powerline fonts uninstalled from $font_dir"
+echo "Powerline symbol font uninstalled from $font_dir"
